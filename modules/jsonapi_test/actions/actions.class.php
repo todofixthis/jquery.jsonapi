22399c8680cf0310b8630e1eee13a365abcc2b9b
Phoenix Zerin <phoenix.zerin@jwt.com>
2011-08-09 10:17:22 -0500

    Added functional tests.

---
+++ modules/jsonapi_test/actions/actions.class.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * jsonapi_test actions.
+ *
+ * @package    jsonapi-tester
+ * @subpackage api
+ * @author     Phoenix Zerin <phoenix.zerin@jwt.com>
+ */
+class jsonapi_testActions extends JsonApi_Actions
+{
+  /** Basic mock action, used to test simple validators.
+   *
+   * @param sfWebRequest $request
+   *
+   * @return string
+   */
+  public function executeBasic( sfWebRequest $request )
+  {
+    $this->requirePost();
+
+    $result = $this->getParam('result', array(
+      new sfValidatorString(array(
+        'required'  => true
+      ))
+    ));
+
+    if( $this->hasErrors() )
+    {
+      return $this->error();
+    }
+
+    return $this->success(array('result' => $result));
+  }
+}
